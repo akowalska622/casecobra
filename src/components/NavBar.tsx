89297bd73385873bc17e3d69aaeec21dbe7f3158
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-13 13:12:05 +0200

    add authentication with kinde

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 
 import { MaxWidthWrapper } from './MaxWidthWrapper';
 import { buttonVariants } from './ui/button';
-import { ArrowRight } from 'lucide-react';
 
-export const NavBar = () => {
-  const user = undefined;
-  const isAdmin = false;
+export const NavBar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
