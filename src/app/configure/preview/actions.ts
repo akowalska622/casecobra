6dd5de8c2687a90aec87c60357881a25291aaaa8
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-24 11:49:56 +0200

    configure stripe payment session

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -5,6 +5,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { db } from '@/db';
 import { BASE_PRICE, PRODUCT_PRICES } from '@/config/product';
 import { Order } from '@prisma/client';
+import { stripe } from '@/lib/stripe';
 
 export const createCheckoutSession = async ({
   configId,
@@ -49,10 +50,41 @@ export const createCheckoutSession = async ({
   } else {
     order = await db.order.create({
       data: {
-        amount: totalPrice / 100,
+        amount: totalPrice,
         userId: user.id,
         configurationId: configuration.id,
       },
     });
   }
+
+  const product = await stripe.products.create({
+    name: 'Custom iPhone Case',
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: 'usd',
+      unit_amount: totalPrice,
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/configure/preview?id=${configId}`,
+    payment_method_types: ['card', 'paypal'],
+    mode: 'payment',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'DE', 'ES', 'FR', 'IT', 'GB', 'CA'],
+    },
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [
+      {
+        price: product.default_price as string,
+        quantity: 1,
+      },
+    ],
+  });
+
+  return { url: stripeSession.url };
 };
